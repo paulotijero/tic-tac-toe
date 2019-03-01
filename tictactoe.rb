00d060bc4aacc863b6540389042bf534a46c804d
paulotijero <paulotijero@gmail.com>
2019-03-01 16:47:00 -0500

    TicTacToe 0.1
    Operativo pero falta validar el empate

--- tictactoe.rb
+++ tictactoe.rb
@@ -30,32 +30,35 @@ COMBINATIONS_WIN = [
   [3,5,7]
 ]
 
-def playerChange(value)
-   (value == "O") ? "X" : "O"
+TABLE = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9
+]
+
+def notPlayed(value)
+  arrNP = TABLE - value
+  return arrNP 
 end
 
-def turnHuman()
+def turnHuman(notplayed)
     value = 0
     loop do
       puts "\nEs el turno del Humano"
       value = gets.chomp.to_i
-      break if ( (value >= 1 ) && (value <= 9 ) )
+      break if ( ( notplayed.include?(value)) && (value >= 1) && (value <=9) )
     end
     return value
 end
 
-def markedValue(arr1,arr2)
-  (arr1 + arr2).uniq
-end
-
-def turnBot(arr)
-  bot_mark = []
-  loop do
-    # rand(1..9)
-    # TODO: Asigna la posicion de marcado del BOT
-    break if (  )
-  end
-  return
+def turnBot(value)
+  value.sample
 end
 
 def tictactoe()
@@ -67,35 +70,36 @@ def tictactoe()
 
   player = "X"
   bot = "O"
-  
 
-  marked = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  table = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   while (band == false) do
-    
-    pos_human = turnHuman()
-    marked[pos_human-1] = player
+
+
+    # pos_human : Es el valor que ingresa el usuario
+    pos_human = turnHuman(notPlayed(value_human+value_bot))
+
+    table[pos_human-1] = player
     value_human.push(pos_human)
     
-    matriz_marked = markedValue(value_human,value_bot)
-    
 
-    pos_bot = turnBot(matriz_marked)
-    marked[pos_bot-1] = bot
+    pos_bot = turnBot(notPlayed(value_human+value_bot))
+    table[pos_bot-1] = bot
     value_bot.push(pos_bot)
 
     # player == "X" ? value_human.push(pos) : value_bot.push(pos)
 
-    puts "\n #{marked[0]} | #{marked[1]} | #{marked[2]} "
+    puts "\n #{table[0]} | #{table[1]} | #{table[2]} "
     puts "-----------"
-    puts " #{marked[3]} | #{marked[4]} | #{marked[5]} "
+    puts " #{table[3]} | #{table[4]} | #{table[5]} "
     puts "-----------"
-    puts " #{marked[6]} | #{marked[7]} | #{marked[8]} "
+    puts " #{table[6]} | #{table[7]} | #{table[8]} "
 
     puts "Soy un humna #{value_human}"
     puts "Soy un bot #{value_bot}"
     
     for i in 0..COMBINATIONS_WIN.length-1
+      
       if ( (COMBINATIONS_WIN[i] - value_human) == [] )
         puts "El indescutible ganador!! es el HUMANO #{player}"
         return band = true 
@@ -103,10 +107,8 @@ def tictactoe()
         puts "El indescutible ganador!! es el BOT #{player}"
         return band = true 
       end
+      
     end
-
-    # player = playerChange(player)
-
 
   end
 
